07557ec1683631de0179384f49d8b5b337e51a0b
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-25 08:26:59 -0400

    Added implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{myHero.name}}</h2>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{hero.name}}
+      </li>
+    </ul>
+    <p *ngIf="heroes.length > 3">There are many heroes</p>
+  `
 })
 export class AppComponent {
-  title = 'displaying-data';
+  title: string = 'Tour of Heroes';
+  myHero: Hero = new Hero(1, 'Windstorm');
+  heroes: Hero[] = [
+    new Hero(1, 'Windstorm'),
+    new Hero(1, 'Bombasto'),
+    new Hero(1, 'Magneta'),
+    new Hero(1, 'Tornado'),
+  ];
 }
