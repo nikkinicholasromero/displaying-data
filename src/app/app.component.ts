3652575e305226fbd8755cd6fcee58af57711ba4
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-26 03:08:39 -0400

    Added a few more property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,12 @@ import { Hero } from './hero';
       </li>
     </ul>
     <p *ngIf="heroes.length > 3">There are many heroes</p>
+    <p>{{doesThisDetectChanges()}}</p>
+    <button (click)="changeTitle()">Click me</button>
+    <input [type]="'text'"/>
+    <input [attr.type]="'text'"/>
+    <input [disabled]="true"/>
+    <input [attr.disabled]="'disabled'"/>
   `
 })
 export class AppComponent {
@@ -24,4 +30,13 @@ export class AppComponent {
     new Hero(1, 'Magneta'),
     new Hero(1, 'Tornado'),
   ];
+
+  doesThisDetectChanges(): string {
+    return this.title;
+  }
+
+  changeTitle(): void {
+    this.title = 'hello';
+  }
+  
 }
